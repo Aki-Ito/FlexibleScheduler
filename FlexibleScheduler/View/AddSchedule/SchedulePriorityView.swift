b5dcc0b475f3f232326b947e83e6102cf5440dd8
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2023-08-08 21:37:06 +0900

    add: priorityView

---
+++ FlexibleScheduler/View/AddSchedule/SchedulePriorityView.swift
@@ -0,0 +1,20 @@
+//
+//  SchedulePriorityView.swift
+//  FlexibleScheduler
+//
+//  Created by 伊藤明孝 on 2023/08/08.
+//
+
+import UIKit
+
+class SchedulePriorityView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
